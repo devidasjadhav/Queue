c21e06175c73d3e991442c8b2482f29c1be10def
devidasjadhav <freekepange@gmail.com>
2018-02-14 21:12:27 +0530

    Optimised for one enqueue and one dequeue back to back.
    Code will perform better in back to back enqueue and dequeue.
    Also added error in case of dequeue empty queue.

--- queue.py
+++ queue.py
@@ -3,22 +3,40 @@ stk1 = []
 stk2 = []
 
 def enque(item):
-  if stk2:
-    while len(stk2) > 0 : stk1.append(stk2.pop())
-  stk1.append(item)
+    if not stk1:
+        stk2.append(item)
+    else:
+        while len(stk2) > 1 : stk1.append(stk2.pop())
+    stk1.append(item)
 
 def deque():
-  if stk1:
-    while len(stk1) > 0 : stk2.append(stk1.pop())
-  return stk2.pop()
+    if not stk2:
+        if not stk1:
+            print "Error Empty Queue"
+            return "Error"
+        else:
+            while len(stk1) > 2:
+                stk2.append(stk1.pop())
+            ret = stk1.pop()
+            stk1.pop()
+            return ret
+    else:
+        return stk2.pop()
 
-###############################################################################################################
-###########################################   TEST QUEUE ######################################################
-###############################################################################################################
 
 print stk1
 print stk2
 print enque(1)
+
+
+print stk1
+print stk2
+print enque(4)
+
+
+print stk1
+print stk2
+print deque()
 
 print stk1
 print stk2
@@ -27,14 +45,6 @@ print enque(2)
 print stk1
 print stk2
 print enque(3)
-
-print stk1
-print stk2
-print enque(4)
-
-print stk1
-print stk2
-print deque()
 
 print stk1
 print stk2
